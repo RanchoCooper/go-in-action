ead5f8195fa2a2bca7ea3dd8bebb6d8af4c2d0b2
Rancho <ranchocooper@gmail.com>
2020-02-13 23:53:28 +0800

    benchmark

---
+++ chapter09/benchmark/benchmark_test.go
@@ -0,0 +1,41 @@
+package benchmark
+
+import (
+	"fmt"
+	"strconv"
+	"testing"
+)
+
+// BenchmarkSprintf provides performance numbers for the
+// fmt.Sprintf function.
+func BenchmarkSpintf(b *testing.B) {
+	number := 1000
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		_ = fmt.Sprintf("%d", number)
+	}
+}
+
+// BenchmarkFormat provides performance numbers for the
+// strconv.FormatInt function.
+func BenchmarkFormat(b *testing.B) {
+	number := int64(1000)
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		strconv.FormatInt(number, 10)
+	}
+}
+
+// BenchmarkItoa provides performance numbers for the
+// strconv.Itoa function.
+func BenchmarkItoa(b *testing.B) {
+	number := 10
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		strconv.Itoa(number)
+	}
+}
