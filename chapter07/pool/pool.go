663823b3f4f259cfe3c987d16db44c548d9d94aa
Rancho <ranchocooper@gmail.com>
2020-03-01 19:45:08 +0800

    up

---
+++ chapter07/pool/pool.go
@@ -0,0 +1,95 @@
+package pool
+
+import (
+	"errors"
+	"io"
+	"log"
+	"sync"
+)
+
+// Pool manages a set of resources that can be shared safely by multiple goroutines.
+// The resource being managed must implement the io.Closer interface.
+type Pool struct {
+	m         sync.Mutex
+	resources chan io.Closer
+	factory   func() (io.Closer, error)
+	closed    bool
+}
+
+var ClosedError = errors.New("pool has been closed")
+
+// New creates a pool that manages resources.
+func New(fn func() (io.Closer, error), size uint) (*Pool, error) {
+	if size <= 0 {
+		return nil, errors.New("size should be a positive number")
+	}
+	return &Pool{
+		resources: make(chan io.Closer, size),
+		factory:   fn,
+	}, nil
+}
+
+// Acquire retrieves a resource from the pool
+func (p *Pool) Acquire() (io.Closer, error) {
+	select {
+	case r, ok := <-p.resources:
+		log.Println("Acquire:", "Shared Resource")
+		if !ok {
+			return nil, ClosedError
+		}
+		return r, nil
+
+	// Provide a new resource since there are none available.
+	default:
+		log.Println("Acquire:", "New Resource")
+		return p.factory()
+	}
+}
+
+// Release places a new resource onto the pool.
+func (p *Pool) Release(r io.Closer) {
+	// Secure this operation with the Close operation.
+	p.m.Lock()
+	defer p.m.Unlock()
+
+	// If the pool is closed, discard the resource.
+	if p.closed {
+		r.Close()
+		return
+	}
+
+	select {
+	// Attempt to place the new resource on the queue.
+	case p.resources <- r:
+		log.Println("Release:", "In Queue")
+
+	// If the queue is already at cap we close the resource.
+	default:
+		log.Println("Release:", "Closing")
+		r.Close()
+	}
+}
+
+// Close will shutdown the pool and close all existing resources.
+func (p *Pool) Close() {
+	// Secure this operation with the Release operation.
+	p.m.Lock()
+	defer p.m.Unlock()
+
+	// If the pool is already close, don't do anything.
+	if p.closed {
+		return
+	}
+
+	// Set the pool as closed.
+	p.closed = true
+
+	// Close the channel before we drain the channel of its
+	// resources. If we don't do this, we will have a deadlock.
+	close(p.resources)
+
+	// Close the resources
+	for r := range p.resources {
+		r.Close()
+	}
+}
